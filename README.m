e54450952c3533ce6607460829a7aa220d4e7318
jonathanponcerodriguez0-art <jonathanponcerodriguez0@gmail.com>
2025-11-12 13:34:53 -0800

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,14 @@
+Solve the following with a MATLAB script. Include an output statement answering the question in the first bullet point.
+The equation for the temperature T of a certain object immersed in a liquid bath of constant temperature, Tb, is
+10 𝑑𝑑𝑑𝑑
+𝑑𝑑𝑑𝑑 + 𝑑𝑑 = 𝑑𝑑𝑏𝑏
+The initial temperature of the object is 70 ̊F and the bath temperature is 170 ̊F.
+• Report the temperature after 10, 20, and 40 seconds
+• How long does it take for the object to reach 160 ̊F? (Give answer to one decimal place.)
+• Plot the function for T(t) for the first 40 seconds. Set the range of the vertical axis from 0 to 180 ̊C.
+Include a grid, title, and axis labels.
+Va
++
+V
+-
 
